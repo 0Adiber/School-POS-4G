c4f092012284ebcce5caad353555c78bf538ad5c
0Adiber <adiber.yt@gmail.com>
2020-12-21 18:30:46 +0100

    :bug: Fixed Page Parameter :sparkles: Load More :art: Basic Listing
    Fixed page parameter (&page != &p)
    Designed the listing a bit
    Button "load more" loads ten more search results
    Put Movies on Session Storage

--- Bsp_205_OpenMovieDatabase/src/main/java/at/htlkaindorf/controller/MovieController.java
+++ Bsp_205_OpenMovieDatabase/src/main/java/at/htlkaindorf/controller/MovieController.java
@@ -4,8 +4,9 @@ import at.htlkaindorf.pojos.Movie;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.ServletException;
@@ -34,28 +35,54 @@ public class MovieController extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
 
-        String search = request.getParameter("search");
+        String search = (String)request.getAttribute("search");
         if (search != null && !search.trim().isEmpty()) {
-            int page = 1;
-
-            JsonNode sNode = mapper.readTree(new URL(URL + "&s=" + search + "&p=" + page));
-
-            List<Movie> movies = new ArrayList<>();
-
-            for (int i = 0; i < 10; i++) {
-                JsonNode rNode = sNode.get("Search").get(i);
-                String id = rNode.get("imdbID").asText();
-                
-                movies.add(mapper.readValue(new URL(URL + "&i=" + id), Movie.class));
+            search = search.trim();
+            request.getSession().setAttribute("search", search);
+            /*int page;
+            try {
+                page = Integer.parseInt(request.getParameter("page"));
+                int maxpage = (int)request.getSession().getAttribute("maxpage");
+                assert page >= 1 && page <= maxpage;
+            }catch(NumberFormatException | AssertionError e) {
+                page = 1;
+            }*/
+            
+            int curres;
+            try {
+                System.out.println("curres: " + request.getParameter("curres"));
+                curres = Integer.parseInt(request.getParameter("curres"));
+            }catch(NumberFormatException e) {
+                curres = 1;
             }
             
-            request.setAttribute("movies", movies);
+            JsonNode sNode = mapper.readTree(new URL(URL + "&s=" + URLEncoder.encode(search) + "&page=" + curres)); //.encode(<String>) is deprecated in Java14, but not Java8
+
+            List<Movie> movies = (List<Movie>)request.getSession().getAttribute("movies");
+            if(movies == null || (request.getSession().getAttribute("search") != null && request.getSession().getAttribute("search") != search)) {
+                movies = new ArrayList<>();
+            }
+            
+            try {
+                sNode.get("Error").asText();
+            }catch(NullPointerException e) {
+                for (int i = 0; i < 10; i++) {
+                    JsonNode rNode = sNode.get("Search").get(i);
+                    String id = rNode.get("imdbID").asText();
+
+                    movies.add(mapper.readValue(new URL(URL + "&i=" + id), Movie.class));
+                }
+            }
+            
+            request.getSession().setAttribute("movies", movies);
+            request.getSession().setAttribute("curres", curres);
+            //request.getSession().setAttribute("page", page);
+            //request.getSession().setAttribute("maxpage", Math.floorDiv(sNode.get("totalResults").asInt(),10));
         }
 
         request.getRequestDispatcher("movieView.jsp").forward(request, response);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -81,6 +108,12 @@ public class MovieController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if(request.getParameter("search") != null) {
+            request.getSession().invalidate();
+            request.setAttribute("search", request.getParameter("search"));
+        } else {
+            request.setAttribute("search", request.getSession().getAttribute("search"));
+        }
         processRequest(request, response);
     }
 
@@ -91,7 +124,7 @@ public class MovieController extends HttpServlet {
      */
     @Override
     public String getServletInfo() {
-        return "Short description";
+        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
     }// </editor-fold>
 
 }
--- Bsp_205_OpenMovieDatabase/src/main/webapp/movieView.jsp
+++ Bsp_205_OpenMovieDatabase/src/main/webapp/movieView.jsp
@@ -5,21 +5,37 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
+        <link rel="stylesheet" href="style.css" />
     </head>
     <body>
+        
+        <h1>Movie Search</h1>
+        
         <form action="./" method="POST">
             <input type="text" name="search" />
             <button type="submit">Search!</button>
         </form>
         
-        <ul>
+        <ul id="movies">
             <c:forEach var="m" items="${movies}">
                 <li>
-                    <img src="${m.poster}" width="150px" />
-                    <p>${m.title}</p>
+                    <div class="movie-small">
+                        <img class="poster" src="${m.poster}" />
+                        <div class="movie-info">
+                            <p class="title">${m.title}</p>
+                        </div>
+                    </div>
                 </li>
             </c:forEach>
         </ul>
+
+        <!-- <button name="page" value="${page-1}">-</button>
+        <span>${page}/${maxpage}</span>
+        <button name="page" value="${page+1}">+</button> -->
+                    
+        <form action="./" method="POST">
+            <button type="submit" name="curres" value="${curres+1}">load more</button> 
+        </form>
 
     </body>
 </html>
